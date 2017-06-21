632f15c855a4b2f61a5c767edd895e57b3d27cd0
kvjqzx <kvjqzx@gmail.com>
2017-06-21 19:46:32 +0800

    Save data to SharedPreference
    The data in SharedPreference are Key-Value sets.
    Can get them by:
    getSharedPreferences() // Get shared preference identified by name.
    getPreferences() // Get a default shared preference that belongs to the activity.
    
    The directory of the file in phone:
    /data/data/com.kvjqzx.androidtraining/shared_prefs/MainActivity.xml
    
    <?xml version='1.0' encoding='utf-8' standalone='yes' ?>
    <map>
        <string name="com.kvjqzx.androidtraining.MESSAGE">Hello World</string>
    </map>

--- app/src/main/java/com/kvjqzx/androidtraining/MainActivity.java
+++ app/src/main/java/com/kvjqzx/androidtraining/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kvjqzx.androidtraining;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,17 +10,28 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.kvjqzx.androidtraining.MESSAGE";
+    private EditText mMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessage = (EditText) findViewById(R.id.editText);
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        mMessage.setText(sp.getString(EXTRA_MESSAGE, "Hi"));
     }
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
+
+        String message = mMessage.getText().toString();
+
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(EXTRA_MESSAGE, message);
+        editor.commit();
+
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
