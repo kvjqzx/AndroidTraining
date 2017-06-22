d0e61db835390e61c366cf209d528faab4933ed5
kvjqzx <kvjqzx@gmail.com>
2017-06-22 15:48:50 +0800

    Save a File in internal storage
    The internal directory of app's:
    /data/data/com.kvjqzx.androidtraining/files/
    /data/data/com.kvjqzx.androidtraining/cache/

--- app/src/main/java/com/kvjqzx/androidtraining/MainActivity.java
+++ app/src/main/java/com/kvjqzx/androidtraining/MainActivity.java
@@ -8,9 +8,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.kvjqzx.androidtraining.MESSAGE";
     private EditText mMessage;
+    private EditText mDir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMessage = (EditText) findViewById(R.id.editText);
+        mDir = (EditText) findViewById(R.id.editText2);
+
         SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
         mMessage.setText(sp.getString(EXTRA_MESSAGE, "Hi"));
     }
@@ -34,5 +42,28 @@ public class MainActivity extends AppCompatActivity {
 
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void GetPath(View view) {
+        File fileDir = getFilesDir();
+        File fileCacheDir = getCacheDir();
+        mDir.setText(fileDir.getAbsolutePath() + "  " + fileCacheDir.getAbsolutePath());
+
+        try {
+            // Create a file test.txt in /data/data/com.kvjqzx.androidtraning/files/
+            File file = new File(getFilesDir(), "test.txt");
+            file.createNewFile();
+
+            // It will create a test2.txt in data directory
+            FileOutputStream out = openFileOutput("test2.txt", MODE_PRIVATE);
+            out.write("Hello world".getBytes());
+            out.close();
+
+            // It will create a file like cache5625271425208450524.tmp in cache directory
+            File cache = File.createTempFile("cache", null, getCacheDir());
+            cache.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
